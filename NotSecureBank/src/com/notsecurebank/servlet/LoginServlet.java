92c1d237177a7810fd3de94e78157c843fd9a5bb
vincenzo <vi.bovino@reply.it>
2023-07-13 12:21:52 +0200

    fix V3_InformationDisclosure

--- NotSecureBank/src/com/notsecurebank/servlet/LoginServlet.java
+++ NotSecureBank/src/com/notsecurebank/servlet/LoginServlet.java
@@ -58,7 +58,7 @@ public class LoginServlet extends HttpServlet {
             password = password.trim();
 
             if (!DBUtil.isValidUser(username, password)) {
-                LOG.error("Login failed >>> User: " + username + " >>> Password: " + password);
+                LOG.error("Login failed");
                 throw new Exception("Login Failed: We're sorry, but this username or password was not found in our system. Please try again.");
             }
         } catch (Exception ex) {
